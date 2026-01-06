55703c67958aa1b50ac7c43b71e35fdada5ddfb1
Haroon Siddique <haroonsiddique97@gmail.com>
2026-01-06 21:51:48 +0500

    build the seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,27 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+10.times do |blog|
+  Blog.create!(
+    title: "this is my Blog post #{blog}",
+    body:"this my Blog post body text#{blog}"
+  )
+end
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Software Engineering #{skill}",
+    percent_utilized: 15,
+  )
+end
+
+puts "5 skills created"
+
+15.times do |portfolio_items|
+  Portfolio.create!(
+    title:"this is my portfolio title #{portfolio_items}",
+    subtitle: "My great service",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "https://via.placeholder.com/600x400",
+    thumb: "https://via.placeholder.com/350x200"
+  )
+end
+puts "15 portfolio items created"
