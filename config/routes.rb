b102f87a850e138b27890258efa405b89ae52957
Haroon Siddique <haroonsiddique97@gmail.com>
2026-01-11 19:27:00 +0500

    build the resource route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [ :show ]
   resources :blogs
 
-  get "contact", to: "pages#contact"
+  get "portfolio/:id", to: "portfolios#show", as: "portfolio_show"
+   get "contact", to: "pages#contact"
   get "about", to: "pages#about"
   root to: "pages#home"
   get "up" => "rails/health#show", as: :rails_health_check
