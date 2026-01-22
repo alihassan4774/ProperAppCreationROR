a6116321936fde4939277cae8588422957590634
Haroon Siddique <haroonsiddique97@gmail.com>
2026-01-22 12:47:13 +0500

    Integrated database relationship between Topic and Blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_20_061706) do
+ActiveRecord::Schema[8.0].define(version: 2026_01_22_064909) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,7 +18,9 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_20_061706) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "status", default: 0
+    t.integer "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -58,4 +60,12 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_20_061706) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "blogs", "topics"
 end
