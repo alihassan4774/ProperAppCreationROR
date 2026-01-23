1fe6722e34cdc6072984fcbb22e2ac799ff2a916
Haroon Siddique <haroonsiddique97@gmail.com>
2026-01-23 12:22:44 +0500

    Implement the Set Defaults value in rails

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,12 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_programming, -> { where(subtitle: "Ruby Programming") }
+
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-Rejw1o-O-KUg6obEIt3jEkabeMY8ldoAmg&s"
+     self.thumb ||= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-Rejw1o-O-KUg6obEIt3jEkabeMY8ldoAmg&s"
+  end
 end
