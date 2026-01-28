511faf2658d4f612c34101eecffc83c58213421a
Haroon Siddique <haroonsiddique97@gmail.com>
2026-01-28 23:07:06 +0500

    Added Session tracker for dynamic source link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,17 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  # Name field ko fill krna ka liya edit page pe
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
+  end
+
+  before_action :set_source
+
+  def set_source
+    session[:source] = params[:q]  if params[:q]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "deain_pages/home"
+  get "deain_pages/restricted"
   devise_for :users, path: "", path_names: { sign_in: "login", sign_out: "logout", sign_up: "register" }
   resources :portfolios, except: [ :show ]
 
