9e6208c869d6a94269d133b0570f537067cc0ab8
Haroon Siddique <haroonsiddique97@gmail.com>
2026-01-29 10:38:46 +0500

    Refactored application controller to include concern for source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  include SetSource
 
   # Name field ko fill krna ka liya edit page pe
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -10,9 +11,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
   end
 
-  before_action :set_source
 
-  def set_source
-    session[:source] = params[:q]  if params[:q]
-  end
 end
