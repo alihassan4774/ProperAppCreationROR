077eaf8b3c42a744503c609ac3eef4883622a88e
Haroon Siddique <haroonsiddique97@gmail.com>
2026-01-29 12:13:22 +0500

    just application controller update for PermissionParameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,5 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   include SetSource
   include DefaultPageContent
-
-  # Name field ko fill krna ka liya edit page pe
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
-  end
+  include PermissionParameters
 end
